8a5d0af5e871b48acc3baeb28b5c796d9a2907c8
lokesh-arya <lokesh2arya@gmail.com>
2025-09-01 22:57:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const eventHandlers = require('./eventHandlers')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
